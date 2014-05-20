a202596634842608372a7d6ef8250e560bb992da
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-05-20 15:48:20 -0400

    updated for writing the feature names

--- matlab/biom2matlab.py
+++ matlab/biom2matlab.py
@@ -8,25 +8,27 @@ biom_fp = "../data/study_550_closed_reference_otu_table_feces.biom"
 map_fp = "../data/mapping_feces.txt"
 label_fp = "labels.txt"
 data_fp = "data.txt"
+feat_fp = "features.txt"
 phenotype = "SEX"
 
 def main():
   data, sample_ids, otus_names = bmu.load_biom(biom_fp)
   meta_data = bmu.load_map(map_fp)
   phenotype_ids = utils.return_phenotypes(sample_ids, meta_data, phenotype)
-  write_labels(phenotype_ids)
-  write_data(data)
+  write_labels(phenotype_ids, label_fp)
+  write_data(data, data_fp)
+  write_labels(otus_names, feat_fp)
   return None 
 
-def write_labels(labels):
-  f = open(label_fp, "w")
+def write_labels(labels, fname):
+  f = open(fname, "w")
   for ell in labels:
     f.write(ell + "\n")
   f.close()
   return None
 
-def write_data(data):
-  f = open(data_fp,"w")
+def write_data(data, fname):
+  f = open(fname,"w")
   for x in data:
     for y in x[:-1]:
       f.write(str(y) + ",")
