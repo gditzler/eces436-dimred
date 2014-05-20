c4c8c4e919037cbc48173d9b17215e5bb751e1cd
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-05-20 01:21:48 -0400

    initial commit

---
+++ matlab/pcoa_gut_mf.m
@@ -0,0 +1,30 @@
+clc;
+clear;
+close all;
+
+addpath('Fathom/')
+
+load data.txt
+labels_stng = importdata('labels.txt');
+
+u = unique(labels_stng);
+for n = 1:length(labels_stng)
+  if strcmp(labels_stng{n}, 'male')
+    labels(n) = 1;
+  else
+    labels(n) = 2;
+  end
+end
+
+dis = f_dis(data, 'hel');
+result = f_pcoa(dis);
+
+figure; 
+hold on;
+box on;
+plot(result.scores(labels == 1, 1), result.scores(labels == 1, 2), 'ro');
+plot(result.scores(labels == 2, 1), result.scores(labels == 2, 2), 'bs');
+legend('male','female');
+xlabel('PC1', 'FontSize', 22);
+ylabel('PC2', 'FontSize', 22);
+set(gca, 'fontsize', 22);
